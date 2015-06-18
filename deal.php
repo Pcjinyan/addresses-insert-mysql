ba9021657f4c0a06210652dbeca35cf1d8d5602e
Pcjinyan <995505775@qq.com>
2015-06-18 16:44:50 +0800

    push code

---
+++ deal.php
@@ -0,0 +1,29 @@
+<?php 
+  /**
+   * @author
+   */
+    //数据库连接
+    $con = new mysqli('localhost','root','pxy123','adbugo');
+    
+    //读取json文件
+    $filename = 'areas.json';
+    $myfile = fopen($filename,'r');
+    $content = fread($myfile,filesize($filename));
+    $de_json = json_decode($content,True);
+    $count_json = count($de_json);
+
+    //循环插入
+    foreach ($de_json as $province => $area1)
+    {
+      foreach ($area1 as $city => $area2 )
+      {
+        foreach ($area2 as $town)
+        {
+          $sql = "INSERT INTO adbugo.addresses (province, city, area) VALUES ('$province', '$city', '$town')";
+          mysqli_query($con, $sql);
+        }
+      }
+    }
+    //关闭数据库
+    mysqli_close($con);
+?>
